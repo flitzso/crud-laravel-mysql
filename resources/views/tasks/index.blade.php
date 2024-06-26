2f8d3259c9ef3aae7514e1004a738019bb5c317b
flitzso <flitzso@hotmail.com>
2024-06-26 09:44:41 -0300

    Crud laravel mysql

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Laravel CRUD</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Laravel CRUD</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-success" href="{{ route('tasks.create') }}"> Create New Task</a>
+                </div>
+            </div>
+        </div>
+
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+        @endif
+
+        <table class="table table-bordered">
+            <tr>
+                <th>No</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th width="280px">Action</th>
+            </tr>
+            @foreach ($tasks as $task)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $task->title }}</td>
+                <td>{{ $task->description }}</td>
+                <td>
+                    <form action="{{ route('tasks.destroy',$task->id) }}" method="POST">
+
+                        <a class="btn btn-info" href="{{ route('tasks.show',$task->id) }}">Show</a>
+
+                        <a class="btn btn-primary" href="{{ route('tasks.edit',$task->id) }}">Edit</a>
+
+                        @csrf
+                        @method('DELETE')
+
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+
+    </div>
+</body>
+
+</html>
